a52fa43737856b5d86ba3e0e50112b6282a6c9d1
sun964158763 <tIANCI9641>
2017-08-28 09:00:28 +0800

    更改数据库信息

--- app/src/main/java/carlife/jsbd/com/myapplication/MainActivity.java
+++ app/src/main/java/carlife/jsbd/com/myapplication/MainActivity.java
@@ -25,5 +25,6 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("libcarlifevehicle");
     }
 }
